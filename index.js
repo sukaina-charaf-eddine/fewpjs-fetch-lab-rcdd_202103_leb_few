0b3a09ddc5fb55ec5c5506be29a04876678fe65f
Sukaina Charaf Eddine <sukaina.kabalan01@gmail.com>
2021-04-21 19:41:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
 }
+
+function renderBooks(json) {
+  const main = document.querySelector('main')
+  json.forEach(book => {
+    const h2 = document.createElement('h2')
+    h2.innerHTML = `<h2>${book.name}</h2>`
+    main.appendChild(h2)
+  })
+}
+document.addEventListener('DOMContentLoaded', function() {
+  fetchBooks()
+})
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -15,6 +30,5 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
-fetch("https://anapioficeandfire.com/api/books")
-.then(resp => resp.json())
-.then(json => console.log(json));
+
+
